9f178dfdc78f9361352e156ac47279c75bd4e3e0
Donald Hioe <donald.hioe@student.hu.nl>
2022-08-19 18:17:15 +0200

    Onnodige code verwijderd + Crud statements voor adresgegevens toegevoegd + begin sorteren/filtereren adresgegevens

--- AdresgegevensAPI/Controllers/AdresgegevensController.cs
+++ AdresgegevensAPI/Controllers/AdresgegevensController.cs
@@ -29,8 +29,97 @@ namespace AdresgegevensAPI.Controllers
                 return NotFound();
             }
             return await _context.Adresgegevens.ToListAsync();
+            //OrderByDescending(x => x.Plaats).ToListAsync();
 
         }
+
+        [HttpGet("filter", Name = "GetAdresgegevensByFilter")]
+        // GET: AdresgegevensByFilter
+        public async Task<ActionResult<IEnumerable<Adresgegeven>>> GetAdresgegevensByFilter(string? straatFilter, long? huisnummerFilter)
+        {
+            var adresgegevens = _context.Adresgegevens;
+
+            if (adresgegevens == null)
+            {
+                return NotFound();
+            }
+
+            var filtered = adresgegevens;
+
+            /*if (straatFilter != null)
+            {
+                filtered.Where(item => item.Straat.Equals(straatFilter));
+            }
+
+            if(huisnummerFilter != null)
+            {
+                filtered.Where(item => item.Huisnummer.Equals(huisnummerFilter.ToString()));
+            }*/
+
+            return await filtered
+                .Where(item => item.Straat.Equals(straatFilter) && item.Huisnummer.Equals(huisnummerFilter.ToString()))                
+                .ToListAsync();
+        }
+
+        [HttpGet("sort", Name = "GetAdresgegevensBySorting")]
+        // GET: AdresgegevensBySorting
+        public async Task<ActionResult<IEnumerable<Adresgegeven>>> GetAdresgegevensBySorting(string sorting)
+        {
+            var adresgegevens = _context.Adresgegevens;
+
+            if (adresgegevens == null)
+            {
+                return NotFound();
+            }
+
+            var sorted = adresgegevens;
+
+            if (sorting.ToLower().Equals("straat"))
+            {
+                return await sorted
+                 .OrderBy(x => x.Straat).ToListAsync();
+            }
+            else if (sorting.ToLower().Equals("huisnummer"))
+            {
+               return await sorted
+                .OrderBy(x => x.Huisnummer).ToListAsync();
+            }          
+            else if (sorting.ToLower().Equals("postcode"))
+            {
+                return await sorted
+                 .OrderBy(x => x.Postcode).ToListAsync();
+            }
+            else if (sorting.ToLower().Equals("plaats"))
+            {
+                return await sorted
+                 .OrderBy(x => x.Plaats).ToListAsync();
+            }
+            else if (sorting.ToLower().Equals("land"))
+            {
+                return await sorted
+                 .OrderBy(x => x.Land).ToListAsync();
+            }
+
+            return BadRequest();
+
+        }
+        [HttpGet("{id}", Name = "GetAdresgegevensById")]
+        // GET: AdresgegevensById
+        public async Task<ActionResult<Adresgegeven>> GetAdresgegevenById(long id)
+        {
+            if(_context.Adresgegevens == null)
+            {
+                return NotFound();
+            }
+            var adresgegevens = await _context.Adresgegevens.FindAsync(id);
+
+            if(adresgegevens == null)
+            {
+                return NotFound();
+            }
+            return adresgegevens;
+        }
+       
         [HttpPost(Name = "PostAdresgegevens")]
         //Post: Adresgegevens
         public async Task<ActionResult<Adresgegeven>> PostAdresgegeven(Adresgegeven adresgegeven)
